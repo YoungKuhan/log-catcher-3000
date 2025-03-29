711382949574b98fdb0bca2d340b18c40c75d8b4
qweasd <qweasd@LAPTOP-JQAG1J26>
2025-03-29 17:20:33 +0100

    Dodana fabryka loggera, żebym mógł dodać aktualnie teraz nLoga oraz zeby można było przekazać swoją konfigurację loggera do biblioteki

---
+++ LogCather3000Library/LoggerFactoryInstance.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.Extensions.Logging;
+using NLog.Extensions.Logging;
+
+public static class LoggerFactoryInstance
+{
+    private static ILoggerFactory? _loggerFactory;
+
+    public static void Configure(ILoggerFactory loggerFactory)
+    {
+        _loggerFactory = loggerFactory;
+    }
+
+    public static ILogger<T> CreateLogger<T>()
+    {
+        if (_loggerFactory == null)
+        {
+            _loggerFactory = LoggerFactory.Create(builder =>
+            {
+                builder.ClearProviders();
+                builder.AddNLog();
+            });
+        }
+        return _loggerFactory.CreateLogger<T>();
+    }
+}
